3e970ec099d4186e436b64e45ce3d0ac6edea05f
Jennifer Lee <jennifer.jlee@outlook.com>
2019-01-18 17:35:50 -0500

    Initial commit

---
+++ src/switchStatement.java
@@ -0,0 +1,11 @@
+/*
+* Create a program for the Hole in Ten Golf Company.
+* It will prompt the user for a product code.
+* The program will then display the corresponding price and description from the table below.
+
+Use switches and number formats!*/
+public class switchStatement {
+    public static void main(String[] args){
+
+    }
+}
